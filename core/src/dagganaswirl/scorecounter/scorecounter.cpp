1b4b2ba15bd822e2ee2d801212463c05add5a92e
c1mmo <kkeronen@gmail.com>
2012-10-13 19:10:10 +0300

    Added comments to ScoreCounter

--- core/src/dagganaswirl/scorecounter/scorecounter.cpp
+++ core/src/dagganaswirl/scorecounter/scorecounter.cpp
@@ -1,9 +1,7 @@
 #include "scorecounter.h"
 #include "../game/model/game.h"
 
-#include <vector>
-
-#define MIN_GROUP_SIZE 4
+#define MIN_GROUP_SIZE 4	//Groups smaller than this do not generate score.
 
 ScoreCounter::ScoreCounter(Game * game)
 {
@@ -13,6 +11,10 @@ ScoreCounter::ScoreCounter(Game * game)
 
 long ScoreCounter::getScore()
 {
+	/**
+	*	Count score before and return value.
+	*/
+	countScore();
 	return m_score;
 }
 
@@ -21,10 +23,18 @@ int boardSize;
 
 void checkCoordinate(const int i, const int j, const GameBoard & board, int & groupSize)
 {
+	/**
+	*	Increase the size of group currently being iterated.
+	*	Mark this coordinate as checked.	
+	*/
 	groupSize++;
 	checked[i][j] = 1;
 	const int pieceType = board.getPiece(i, j);
 	
+	/**
+	*	Check if any of the adjacent coordinates contain same type of piece and
+	*	recursively call this function for that coordinate.
+	*/
 	if (i > 0 && checked[i-1][j] == 0 && board.getPiece(i-1, j) == pieceType)
 	{
 		checkCoordinate(i-1, j, board, groupSize);
@@ -48,6 +58,10 @@ void checkCoordinate(const int i, const int j, const GameBoard & board, int & gr
 
 void ScoreCounter::countScore()
 {
+	/**
+	*	Initialize score counting. Generate 2D array of zeroes to represent checked
+	*	coordinates.
+	*/	
 	m_score = 0;
 	boardSize = (int)m_game->getBoard().getSize();
 	checked = new int*[boardSize];
@@ -65,6 +79,9 @@ void ScoreCounter::countScore()
 		}
 	}
 	
+	/**
+	*	Iterate over the gameboard to check groups.
+	*/
 	for (int i = 0; i < boardSize; i++)
 	{
 		for (int j = 0; j < boardSize; j++)
@@ -81,6 +98,9 @@ void ScoreCounter::countScore()
 		}
 	}
 	
+	/**
+	*	Remember to delete allocated 2D array.
+	*/
 	for (int i = 0; i < boardSize; i++)
 	{
 		delete [] checked[i];
--- core/src/dagganaswirl/scorecounter/scorecounter.h
+++ core/src/dagganaswirl/scorecounter/scorecounter.h
@@ -2,14 +2,24 @@
 
 class Game;
 
+/**
+*	Class to count game score. 
+*/
 class ScoreCounter
 {
 public:
 	ScoreCounter(Game * game);
 	virtual ~ScoreCounter() {}
 	
+	/**
+	*	Get current game score.
+	*/
 	long getScore();
 	
+private:
+	/**
+	*	Tell ScoreCounter to count.
+	*/
 	void countScore();
 	
 private:
